21f5c1d72661eea7391ad278ff02c9b4e633ad91
Ciaran Sanders <ciaransanders@gmail.com>
2025-09-18 11:38:22 -0700

    fix: select2 autocomplete init

--- src/unfold/static/unfold/js/select2.init.js
+++ src/unfold/static/unfold/js/select2.init.js
@@ -1,52 +1,45 @@
 "use strict";
 {
-  const $ = django.jQuery;
+    const $ = django.jQuery;
 
-  $.fn.djangoCustomSelect2 = function () {
-    $.each(this, function (i, element) {
-      if (element.id.match(/__prefix__/)) {
-        return;
-      }
+    $.fn.djangoCustomSelect2 = function () {
+        $.each(this, function (i, element) {
+            if (element.id.match(/__prefix__/)) {
+                return;
+            }
 
-      if ($(element).hasClass("select2-hidden-accessible")) {
-        return;
-      }
+            if ($(element).hasClass("select2-hidden-accessible")) {
+                return;
+            }
 
-      $(element).select2();
+            $(element).select2();
+        });
+
+        return this;
+    };
+
+    $.fn.djangoFilterSelect2 = function () {
+        $.each(this, function (i, element) {
+            $(element).select2({
+                ajax: {
+                    data: (params) => {
+                        return {
+                            term: params.term,
+                            page: params.page,
+                            app_label: element.dataset.appLabel,
+                            model_name: element.dataset.modelName,
+                            field_name: element.dataset.fieldName,
+                        };
+                    },
+                },
+            });
+        });
+        return this;
+    };
+
+    $(function () {
+        $(".unfold-admin-autocomplete.admin-autocomplete").djangoCustomSelect2();
+
+        $(".unfold-filter-autocomplete.admin-autocomplete").djangoFilterSelect2();
     });
-
-    return this;
-  };
-
-  $.fn.djangoAdminSelect2 = function () {
-    $.each(this, function (i, element) {
-      $(element).select2({
-        ajax: {
-          data: (params) => {
-            return {
-              term: params.term,
-              page: params.page,
-              app_label: element.dataset.appLabel,
-              model_name: element.dataset.modelName,
-              field_name: element.dataset.fieldName,
-            };
-          },
-        },
-      });
-    });
-    return this;
-  };
-
-  $(function () {
-    $(".unfold-admin-autocomplete.admin-autocomplete").djangoCustomSelect2();
-
-    $(".admin-autocomplete")
-      .not(".unfold-admin-autocomplete")
-      .not("[name*=__prefix__]")
-      .djangoAdminSelect2();
-  });
-
-  document.addEventListener("formset:added", (event) => {
-    $(event.target).find(".admin-autocomplete").djangoAdminSelect2();
-  });
 }
